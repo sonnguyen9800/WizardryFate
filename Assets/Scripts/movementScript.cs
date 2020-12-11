46e288f7ffc3d31ddb220c81afe39bfe83665c66
kasturez <tinhuynhvinh733@gmail.com>
2020-12-11 17:58:17 +0700

    Cannot flip

--- Assets/Scripts/movementScript.cs
+++ Assets/Scripts/movementScript.cs
@@ -7,13 +7,16 @@ public class movementScript : MonoBehaviour
     [SerializeField]
     float speed = 10f;
     float control = 0f;
+    float jumpCount = 0f;
     Rigidbody2D mybody;
 
     public WizardState.State state = WizardState.State.IDLE;
 
+    private Camera main ;
     void Awake()
     {
         mybody = gameObject.GetComponent<Rigidbody2D>();
+        main = Camera.main;
     }
     // Start is called before the first frame update
     void Start()
@@ -24,8 +27,39 @@ public class movementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Running();
+        Jumping();
+        FlipOnMouse();
+    }
+
+    private void FlipOnMouse(){
+        //Vector3 pos = Input.mousePosition;
+        Vector3 mouseposition = Input.mousePosition; //Find Mouse Position
+        mouseposition.z = main.nearClipPlane;
+
+        Vector3 pos2 = main.ScreenToWorldPoint(mouseposition);
+        Debug.Log("DATA" + pos2);
+        if(pos2.x >= gameObject.transform.position.x)
+        {
+            transform.localScale = new Vector3(1, 1,1);
+        }
+        else if(pos2.x < gameObject.transform.position.x)
+        {
+            //transform.rotation = Quaternion.Euler(0,0,0);
+            transform.localScale = new Vector3(-1, 1,1);
+           
+        }
+    }
+
+    private void Jumping(){
+        if ( Input.GetKeyDown(KeyCode.Space)){
+            mybody.AddForce(new Vector2(mybody.velocity.x, 5f), ForceMode2D.Impulse);
+            
+        }
+    }
+
+    private void Running(){
         float control = Input.GetAxis("Horizontal") ;
-        
         if (control !=  0) {
             //acceleration
             speed += 0.1f;
@@ -46,7 +80,6 @@ public class movementScript : MonoBehaviour
         } else {
             state = WizardState.State.RUNNING;
         }
-
-
     }
+
 }
