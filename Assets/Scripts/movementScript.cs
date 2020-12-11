a7d805232a7c6886fd26c2914bf1792fe2e0fcc6
kasturez <tinhuynhvinh733@gmail.com>
2020-12-11 16:18:50 +0700

    Move horizontally

---
+++ Assets/Scripts/movementScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 10f;
+    float control = 0f;
+    Rigidbody2D mybody;
+
+    void Awake()
+    {
+        mybody = gameObject.GetComponent<Rigidbody2D>();
+        // if (mybody == null){
+        //     print("Error");
+        // }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float control = Input.GetAxis("Horizontal") ;
+        
+        if (control !=  0) {
+            //acceleration
+            speed += 0.1f;
+        } else {
+            // deceleration
+            speed -= 0.1f;
+            if (speed < 0) {
+                speed = 0;
+            }
+        }
+        speed = Mathf.Clamp(speed, -2.5f, 2.5f);
+        Vector2 playerVector = new Vector2(control*speed, mybody.velocity.y);
+        mybody.velocity = playerVector;
+    }
+}
