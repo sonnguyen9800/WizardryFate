5e20db460ba88c4f2b55b8a0721bc317f160c24c
kasturez <tinhuynhvinh733@gmail.com>
2020-12-11 18:18:09 +0700

    fix

--- Assets/Scripts/movementScript.cs
+++ Assets/Scripts/movementScript.cs
@@ -12,16 +12,16 @@ public class movementScript : MonoBehaviour
 
     public WizardState.State state = WizardState.State.IDLE;
 
-    private Camera main ;
+    private Camera main;
     void Awake()
     {
-        mybody = gameObject.GetComponent<Rigidbody2D>();
+        mybody = GetComponent<Rigidbody2D>();
         main = Camera.main;
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -32,52 +32,50 @@ public class movementScript : MonoBehaviour
         FlipOnMouse();
     }
 
-    private void FlipOnMouse(){
-        //Vector3 pos = Input.mousePosition;
-        Vector3 mouseposition = Input.mousePosition; //Find Mouse Position
-        mouseposition.z = main.nearClipPlane;
+    private void FlipOnMouse()
+    {
+        Vector3 mousePosition = main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10));
+        transform.localScale = mousePosition.x > transform.position.x ? new Vector3(1,1,1) : new Vector3(-1,1,1);
+        print(mousePosition);   
+    }
 
-        Vector3 pos2 = main.ScreenToWorldPoint(mouseposition);
-        Debug.Log("DATA" + pos2);
-        if(pos2.x >= gameObject.transform.position.x)
+    private void Jumping()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.localScale = new Vector3(1, 1,1);
-        }
-        else if(pos2.x < gameObject.transform.position.x)
-        {
-            //transform.rotation = Quaternion.Euler(0,0,0);
-            transform.localScale = new Vector3(-1, 1,1);
-           
+            mybody.AddForce(new Vector2(0, 5f), ForceMode2D.Impulse);
+
         }
     }
 
-    private void Jumping(){
-        if ( Input.GetKeyDown(KeyCode.Space)){
-            mybody.AddForce(new Vector2(mybody.velocity.x, 5f), ForceMode2D.Impulse);
-            
-        }
-    }
-
-    private void Running(){
-        float control = Input.GetAxis("Horizontal") ;
-        if (control !=  0) {
+    private void Running()
+    {
+        float control = Input.GetAxis("Horizontal");
+        if (control != 0)
+        {
             //acceleration
             speed += 0.1f;
-        } else {
+        }
+        else
+        {
             // deceleration
             speed -= 0.1f;
-            if (speed < 0) {
+            if (speed < 0)
+            {
                 speed = 0;
             }
         }
         speed = Mathf.Clamp(speed, -2.5f, 2.5f);
-        Vector2 playerVector = new Vector2(control*speed, mybody.velocity.y);
+        Vector2 playerVector = new Vector2(control * speed, mybody.velocity.y);
         mybody.velocity = playerVector;
 
 
-        if (control*speed == 0){
+        if (control * speed == 0)
+        {
             state = WizardState.State.IDLE;
-        } else {
+        }
+        else
+        {
             state = WizardState.State.RUNNING;
         }
     }
