f4519ff09ef10125b11d1bb47c4166889e7782a6
Son Nguyen <sonnguyen9800@gmail.com>
2020-12-15 23:39:21 +0700

    Finish Vertical Collision

---
+++ Assets/Scripts/Wizard/Wizard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Controller2D))]
+public class Wizard : MonoBehaviour
+{
+
+    [SerializeField][Range(0,-40)] float gravity = -20;
+    Controller2D controller2D;
+
+    Vector2 movementVelocity ; // Important variable, determine the velocity of game object
+
+
+    private void Awake() {
+        controller2D = GetComponent<Controller2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementVelocity.y += gravity*Time.deltaTime;
+        controller2D.Move(movementVelocity*Time.deltaTime);
+    }
+}
