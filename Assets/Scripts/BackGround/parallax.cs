cdb58dd1412b0a81b362073cc716a33bc2c86517
kasturez <tinhuynhvinh733@gmail.com>
2020-12-17 21:52:06 +0700

    fix

--- Assets/Scripts/BackGround/parallax.cs
+++ Assets/Scripts/BackGround/parallax.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class parallax : MonoBehaviour
+public class Parallax : MonoBehaviour
 {
     float length, startPos;
     [SerializeField]
