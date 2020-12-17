a9a7f2c942739dc33b8fcf349d639b58c3032c4e
kasturez <tinhuynhvinh733@gmail.com>
2020-12-17 21:49:30 +0700

    fix

--- Assets/Scripts/BackGround/parallax.cs
+++ Assets/Scripts/BackGround/parallax.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parralax : MonoBehaviour
+public class parallax : MonoBehaviour
 {
+    float length, startPos;
+    [SerializeField]
+    GameObject cam;
+    [SerializeField]
+    float parallaxEffect;
+    // Start is called before the first frame update
     void Start()
     {
-        // startpos = transform.position.x;
-        // length = GetComponent<SpriteRenderer>().bounds.size.x;
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
-    void Update()
+
+    void FixedUpdate()
     {
-        // float dist = (cam.transform.position.x * parallaxEffect);
-        // transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        float temp = cam.transform.position.x *(1-parallaxEffect);
+        float dist = cam.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+        if (temp > startPos + length){
+            startPos += length;
+        } else if (temp < startPos - length){
+            startPos -= length;
+        }
     }
 }
