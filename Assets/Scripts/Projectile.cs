0ae483eec0a0dd3ffe90d919a80054e35a679099
kasturez <tinhuynhvinh733@gmail.com>
2020-12-29 15:43:24 +0700

    Update Projectile.cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,41 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Origin {
-
-public class Projectile : MonoBehaviour
+namespace Origin
 {
 
-    [SerializeField][Range(0,0.6f)] public float step;
-    private Vector3 targetPosition;
-    private bool isFired = false;
-    // Start is called before the first frame update
-    void Start()
+    public class Projectile : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (targetPosition == null) return;
-        //transform.Translate(Vector3.right* Time.deltaTime*10);
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
-
-        if (transform.position == targetPosition){
-        Destroy(gameObject);
+        [SerializeField] [Range(0, 0.6f)] public float step;
+        private Vector3 targetPosition;
+        private bool isFired = false;
+        // Start is called before the first frame update
+        void Start()
+        {
 
         }
 
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            if (targetPosition == null) return;
+            //transform.Translate(Vector3.right* Time.deltaTime*10);
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
 
-    public void setTarget(Vector3 target){
-        this.targetPosition = target;
-    }
+            if (transform.position == targetPosition)
+            {
+                Destroy(gameObject);
 
-    public void setAngle(float angle){
-        this.transform.rotation = Quaternion.Euler(0,0,angle - 90);
+            }
+
+        }
+
+        public void setTarget(Vector3 target)
+        {
+            this.targetPosition = target;
+        }
+
+        public void setAngle(float angle)
+        {
+            this.transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+        }
     }
-}
 
 }
