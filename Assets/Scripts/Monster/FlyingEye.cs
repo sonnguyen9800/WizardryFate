617e08fb0b2f53a4dce291dcd1a59cb6ada952cf
sonnguyen9800 <sonnguyen9800@gmail.com>
2021-02-10 20:21:21 +0700

    Add A* path finding project

--- Assets/Scripts/Monster/FlyingEye.cs
+++ Assets/Scripts/Monster/FlyingEye.cs
@@ -8,18 +8,24 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterType))]
 [RequireComponent(typeof(SpriteRenderer))]
 
-public class FlyingEye : Monster
+public class FlyingEye : MonoBehaviour
 {
     // Start is called before the first frame update
     private Damageable damageable;
 
 
-
-    [Header("Unique")]
     public CharacterType type = CharacterType.MONSTER;
     private SpriteRenderer _spriteRenderer;
 
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
 
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+    public Transform player;
 
     private void Awake() {
         damageable = GetComponent<Damageable>();
@@ -27,16 +33,49 @@ public class FlyingEye : Monster
 
     }
 
-    private void Update() {
-        if (this.stun > 0) return ;
-
-    }
     void Start()
     {
-      // damageable.OnDead += Die;
+        // damageable.OnDead += Die;
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBtwShots = startTimeBtwShots;
     }
 
-    void Die(){
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+
+    void Die()
+    {
         Destroy(gameObject);
     }
+
+
+
+
+
+
 }
