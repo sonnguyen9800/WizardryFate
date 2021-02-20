b21e947c01b2e3b0a705cd115562cfe6da075346
sonnguyen9800 <sonnguyen9800@gmail.com>
2021-02-20 23:48:03 +0700

    Prepare first item

--- Assets/Scripts/Item/ItemFactory.cs
+++ Assets/Scripts/Item/ItemFactory.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemFactory : MonoBehaviour
+
+[CreateAssetMenu(fileName = "ItemFactory", menuName = "Item Factory", order = 1)]
+
+public class ItemFactory : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class Item{
+        public string name;
+        public GameObject prefab;
+        public float hpRecover;
+        public float amourIncrease;
+        public float damageIncrease;
+        public float increaseHealth;
+        public float dropRate;
+
+    }
+    private Dictionary<string, Item> itemMap = new Dictionary<string, Item>();
+
+    [SerializeField]
+    public Item[] items;
+
+    private void OnEnable()
     {
-        
+        foreach (var item in items)
+        {
+            itemMap[item.name] = item;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
