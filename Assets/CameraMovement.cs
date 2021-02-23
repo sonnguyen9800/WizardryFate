20018670bebea0e704880b49ac82c6066f92fcbd
sonnguyen9800 <sonnguyen9800@gmail.com>
2021-02-23 22:07:56 +0700

    Editing MenuScene

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Camera _mainCamera;
+
+    [SerializeField] float pixelMovementSpeed = 1.2f;
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        _mainCamera.transform.Translate(new Vector3(pixelMovementSpeed, 0, 0) * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Scene/SceneChanger.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//public class SceneName
+//{
+//    private SceneName(string value) { Value = value; }
+
+//    public string Value { get; set; }
+
+//    public static SceneName MainScene   { get { return new SceneName("MainScene"); } }
+//    public static SceneName PlayScene   { get { return new SceneName("PlayScene"); } }
+//    public static SceneName TutorialScene    { get { return new SceneName("TutorialScene"); } }
+//    public static SceneName DefeatScene { get { return new SceneName("DefeatScene"); } }
+//    public static SceneName VictoryScene   { get { return new SceneName("VictoryScene"); } }
+//}
+
+public enum SceneName
+{
+    MainScene,
+    PlayScene,
+    TutorialScene,
+    DefeatScene,
+    VictoryScene
+}
+
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public SceneName sceneName;
+    // Start is called before the first frame update
+
+    void SwitchScene(SceneName sceneName)
+    {
+        SceneManager.LoadScene((int)sceneName);
+    }
+
+  
+}
