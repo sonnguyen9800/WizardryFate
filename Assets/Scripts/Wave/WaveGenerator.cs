898ca8f6713c850840f276092004dbeee77d53e5
sonnguyen9800 <sonnguyen9800@gmail.com>
2021-02-28 08:43:14 +0700

    Finish Wave Generator

--- Assets/Scripts/Wave/WaveGenerator.cs
+++ Assets/Scripts/Wave/WaveGenerator.cs
@@ -2,52 +2,105 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class MonsterPool
+{
+    [SerializeField] public GameObject monsterPrefab;
+    [SerializeField] public int number;
+
+}
+
+// Extension for List
+public static class CollectionExtension
+{
+    private static System.Random rng = new System.Random();
+
+    public static T RandomElement<T>(this IList<T> list)
+    {
+        return list[rng.Next(list.Count)];
+    }
+
+    public static T RandomElement<T>(this T[] array)
+    {
+        return array[rng.Next(array.Length)];
+    }
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
+
 public class WaveGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField]
-    GameObject[] monstersPrefab;
 
-    int difficultLevel = 1; // Increased by time, each level of difficulity increased the spawn rate
+    [SerializeField]
+    MonsterPool[] MonsterPool;
+
+    List<GameObject> Pools = new List<GameObject>();
+
     [SerializeField] private float  _range = 10f; // Range to spawn monster; only spawn when player is near
 
     private float _currentTimer = 0; // Timer reache a threshold -> increase difficultity level
 
     [SerializeField] private float InitialSpawnInterval = 10f;
-    [SerializeField] private float _thresholdTime = 30f;
-
-
-    private float _spawnTimer = 0;
-    private float _spawnTimerRate = 0; // Timer interval between each spawn
 
     private GameObject _player ;
     System.Random rand = new System.Random();  
 
     private void Awake() {
         _player = GameObject.FindGameObjectWithTag("Player");
-        _spawnTimerRate = InitialSpawnInterval;
+        
     }
 
-    private void reduceTimeIntervalBetweenSpawn(){
-        if (_spawnTimerRate <4f) return;
-        _spawnTimerRate = _spawnTimerRate * Mathf.Log(1 / difficultLevel);
+
+    private void Start()
+    {
+        foreach (var monstertype in MonsterPool)
+        {
+            for (int i = 0; i < monstertype.number; i++)
+            {
+                GameObject monsterCreated = Instantiate(monstertype.monsterPrefab, transform.position, Quaternion.identity);
+                monsterCreated.SetActive(false);
+                Pools.Add(monsterCreated);
+            }
+        }
+
+        Pools.Shuffle();
+
     }
+
+
     // Update is called once per frame
     void Update()
     {
+        if (Vector2.Distance(transform.position, _player.transform.position) > _range) return;
         _currentTimer += Time.deltaTime;
-        if (_currentTimer >= _thresholdTime){
-            difficultLevel ++;
-            _currentTimer=0;
-            reduceTimeIntervalBetweenSpawn();
+        if (_currentTimer >= InitialSpawnInterval)
+        {
+            SummonMonster();
+            _currentTimer = 0;
         }
+    }
 
-        if (_spawnTimer > 0) { _spawnTimer -= Time.deltaTime; return;}
-        if (_player == null) return;
-        if (Vector2.Distance(transform.position, _player.transform.position) <= _range){
-            Instantiate(monstersPrefab[rand.Next(monstersPrefab.Length)], transform.position, Quaternion.identity);
-            _spawnTimer = _spawnTimerRate;
+    void SummonMonster()
+    {
+        if (Pools.Count == 0)
+        {
+            Destroy(gameObject);
+            return;
         }
+        GameObject monsterCreated = Pools.RandomElement();
+        Pools.Remove(monsterCreated);
+        monsterCreated.SetActive(true);
     }
 
     private void OnDrawGizmos() {
